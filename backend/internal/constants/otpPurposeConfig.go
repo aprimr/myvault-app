6ce3cfcd96596c9c21993a8e0af1f8b7b71057a9
Aprim <aprimregmi24@gmail.com>
2026-05-14 18:56:01 +0545

    add: otp constants

---
+++ backend/internal/constants/otpPurposeConfig.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	OTPPurposeRegister       = "register"
+	OTPPurposeForgotPassword = "forgot_password"
+	OTPPurposeDeleteAccount  = "delete_account"
+	OTPPurposeChangeEmail    = "change_email"
+)
