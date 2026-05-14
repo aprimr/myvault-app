ce9411eb2e4098c8b446dd100fdde32999443fb9
Aprim <aprimregmi24@gmail.com>
2026-05-14 10:03:01 +0545

    add: otp util

---
+++ backend/internal/util/otp.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+func GenerateOTP() (string, error) {
+	const length = 6
+	const digits = "0123456789"
+
+	otp := make([]byte, length)
+
+	for i := range otp {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(digits))))
+
+		if err != nil {
+			return "", err
+		}
+
+		otp[i] = digits[num.Int64()]
+	}
+
+	return string(otp), nil
+}
