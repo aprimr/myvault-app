76f101613d31a410e53c45aa22fc9e103115b201
Aprim <aprimregmi24@gmail.com>
2026-05-22 20:47:31 +0545

    fix: credential model

--- backend/internal/models/credential.go
+++ backend/internal/models/credential.go
@@ -1,14 +1,15 @@
 package models
 
+import "time"
+
 type Credential struct {
-	Id                   string `json:"id"`
-	Uid                  string `json:"uid"`
-	Title                string `json:"title"`
-	EmailOrUsername      string `json:"email_or_username"`
-	LoginURL             string `json:"login_url"`
-	PasswordEncrypted    string `json:"password_encrypted"`
-	DescriptionEncrypted string `json:"description_encrypted"`
-	LastUsedAt           string `json:"lastused_at"`
-	CreatedAt            string `json:"created_at"`
-	UpdatedAt            string `json:"updated_at"`
+	Id                   string    `json:"id"`
+	Uid                  string    `json:"uid"`
+	Title                string    `json:"title"`
+	EmailOrUsername      string    `json:"email_or_username"`
+	PasswordEncrypted    string    `json:"-"`
+	LoginURL             *string   `json:"login_url"`
+	DescriptionEncrypted *string   `json:"-"`
+	CreatedAt            time.Time `json:"created_at"`
+	UpdatedAt            time.Time `json:"updated_at"`
 }
